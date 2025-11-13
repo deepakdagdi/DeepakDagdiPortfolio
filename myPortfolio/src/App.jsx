33a6c65a509133634e881b187d95395781bb1485
deepak <deepakdagdi.12@gmail.com>
2025-11-13 11:47:57 +0530

    Initial commit: setup React project with Tailwind CSS v3 and Vit

---
+++ myPortfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1 class="text-3xl font-bold underline text-blue-600">
+  Hello Tailwind v3 + Vite!
+</h1></div>
+  )
+}
+
+export default App
